0462eb90d8514b71bdc2bb4cfb0b3d7450dcc65b
iyeee <1009699416@qq.com>
2023-03-04 21:00:55 +0800

    backend

--- res/5 瑞吉外卖项目优化篇/资料/day02/rw_demo/src/main/java/com/itheima/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.itheima.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.itheima.entity.User;
-
-public interface UserService extends IService<User> {
-}
