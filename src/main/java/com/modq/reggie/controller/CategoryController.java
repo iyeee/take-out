1447c32fd16daa7427315eac5580439718d8ce0c
iyeee <1009699416@qq.com>
2023-03-04 21:40:08 +0800

    add CategoryController cache

--- src/main/java/com/modq/reggie/controller/CategoryController.java
+++ src/main/java/com/modq/reggie/controller/CategoryController.java
@@ -8,6 +8,8 @@ import com.modq.reggie.service.CategoryService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.annotations.Delete;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -28,6 +30,7 @@ public class CategoryController {
      * @return
      */
     @PostMapping
+    @CacheEvict(value = "category",allEntries = true)
     public R<String> save(@RequestBody Category category){
         log.info("category:{}",category);
         categoryService.save(category);
@@ -45,6 +48,7 @@ public class CategoryController {
     }
 
     @DeleteMapping
+    @CacheEvict(value = "category",allEntries = true)
     public R<String> delete(@RequestParam("ids") Long id){
         log.info("删除info，id为{}",id);
         categoryService.remove(id);
@@ -52,12 +56,14 @@ public class CategoryController {
     }
 
     @PutMapping
+    @CacheEvict(value = "category",allEntries = true)
     public R<String> update(@RequestBody Category category){
         log.info("修改分类信息:{}",category);
         categoryService.updateById(category);
         return  R.success("分类修改成功");
     }
     @GetMapping("/list")
+    @Cacheable(value = "category",key = "#category.type")
     public R<List<Category>> list(Category category){
         LambdaQueryWrapper<Category> queryWrapper=new LambdaQueryWrapper<>();
         queryWrapper.eq(category.getType()!=null,Category::getType,category.getType());
